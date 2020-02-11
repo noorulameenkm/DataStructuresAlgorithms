1538484bf7f7a5f76b1cebdf52c0d05106a1a478
lakdivian <noorul.km@people10.com>
2020-02-11 21:49:37 +0530

    Added program for finding the number of leaves recursively and iteratively

---
+++ BinaryTree/find_number_of_leaves_binary_tree_iterative.py
@@ -0,0 +1,47 @@
+class Node:
+    """docstring for Node."""
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def getNumberOfLeavesIterative(root):
+    if not root:
+        return 0
+
+    count = 0
+    
+    queue = [root]
+    while len(queue) > 0:
+        node = queue.pop(0)
+
+        if not node.left and not node.right:
+            count += 1
+        else:
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    
+    return count
+
+
+def Main():
+    root = Node(10)
+    root.left = Node(2)
+    root.right = Node(4)
+    root.left.left = Node(5)
+    root.left.right = Node(6)
+    root.right.left = Node(11)
+    root.right.right = Node(12)
+    root.left.left.left = Node(21)
+    root.left.left.right = Node(20)
+    root.left.right.left = Node(61)
+    root.left.right.right = Node(65)
+    n = getNumberOfLeavesIterative(root)
+    print(n)
+
+
+if __name__ == '__main__':
+    Main()
+
