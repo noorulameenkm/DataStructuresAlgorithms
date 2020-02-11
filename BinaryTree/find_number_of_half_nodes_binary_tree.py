342cf855390e1485b1b4958ed53ef8bb1c5c98e6
lakdivian <noorul.km@people10.com>
2020-02-11 22:26:46 +0530

    Added program for finding the number of half nodes by recursively and iteratively

---
+++ BinaryTree/find_number_of_half_nodes_binary_tree.py
@@ -0,0 +1,37 @@
+class Node:
+    """docstring for Node."""
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def getNumberOfHalfNodes(root):
+    if not root:
+        return 0
+    
+    if (root.left and not root.right) or (not root.left and root.right):
+        return 1 + getNumberOfHalfNodes(root.left) + getNumberOfHalfNodes(root.right)
+    
+    else:
+        return getNumberOfHalfNodes(root.left) + getNumberOfHalfNodes(root.right)
+    
+    
+
+def Main():
+    root = Node(10)
+    root.left = Node(2)
+    root.right = Node(4)
+    root.left.left = Node(5)
+    root.left.right = Node(6)
+    root.right.left = Node(11)
+    root.right.right = Node(12)
+    root.left.left.left = Node(21)
+    root.left.left.right = Node(20)
+    root.left.right.left = Node(61)
+    # root.left.right.right = Node(65)
+    n = getNumberOfHalfNodes(root)
+    print(n)
+
+
+if __name__ == '__main__':
+    Main()
