920de3ed2d706f683bd994b735489fe3071deadd
kmameen <ameen.km@hpe.com>
2021-07-11 21:02:26 +0530

    updated decode string solution with dynamic programming tabulation method

--- String/decodeString.py
+++ String/decodeString.py
@@ -40,6 +40,41 @@ def decode_string_memoization(digits):
     return dfs(0)
 
 
+"""
+ Time Complexity - O(N)
+ Space Complexity - O(N)
+"""
+def decode_string_tabulation(digits):
+    length = len(digits)
+
+    def decode_two_digit(previous, current):
+        if previous == '1':
+            return 1
+        
+        if previous == '2' and (current >= '0' and current <= '6'):
+            return 1
+        
+        return 0
+
+    def decode_single(current):
+        if current == '0':
+            return 0
+        
+        return 1
+
+    dp = [0 for _ in range(length + 1)]
+    dp[0] = 1
+    dp[1] = decode_single(digits[0])
+    for i in range(2, length + 1):
+        previous = digits[i - 2]
+        current = digits[i - 1]
+
+        dp[i] += (dp[i - 1] * decode_single(current))
+        dp[i] += (dp[i - 2] * decode_two_digit(previous, current))
+    
+    return dp[length]
+
+
 # FIRST FUNCTION
 print(decode_ways('18'))
 print(decode_ways('123'))
@@ -50,6 +85,12 @@ print(decode_ways('11223'))
 print(decode_string_memoization('18'))
 print(decode_string_memoization('123'))
 print(decode_string_memoization('11223'))
+
+
+# Third Method
+print(decode_string_tabulation('18'))
+print(decode_string_tabulation('123'))
+print(decode_string_tabulation('11223'))
 
 
 
