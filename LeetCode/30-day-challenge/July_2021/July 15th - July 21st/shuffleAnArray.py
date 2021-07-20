312a523859996b3bfadc284c5197a47857ff7608
kmameen <ameen.km@hpe.com>
2021-07-20 16:50:24 +0530

    Added solution for july 20th 2021 leetcode challenge

---
+++ LeetCode/30-day-challenge/July_2021/July 15th - July 21st/shuffleAnArray.py
@@ -0,0 +1,32 @@
+"""
+    Problem Link:- https://leetcode.com/problems/shuffle-an-array/
+"""
+
+import random
+class Solution:
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.nums
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        """
+        randomArray = list(self.nums)
+        length = len(randomArray)
+        for i in range(length - 1, -1, -1):
+            randomIndex = random.randint(0, i)
+            copy = randomArray[i]
+            randomArray[i] = randomArray[randomIndex]
+            randomArray[randomIndex] = copy
+        
+        return randomArray
+
+
+
