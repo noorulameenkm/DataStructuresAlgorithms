60a74934bfada6c7408ae09e393b90edf193a4fe
kmameen <ameen.km@hpe.com>
2021-07-31 23:01:14 +0530

    Added solution for july 31st 2021 leetcode challenge

--- LeetCode/30-day-challenge/December/December 15th - December 21st/tripletSequence.py
+++ LeetCode/30-day-challenge/December/December 15th - December 21st/tripletSequence.py
@@ -1,5 +1,7 @@
 import math
 
+
+# Check triplet sequence exits
 def isTripletSequenceExists(nums):
     if len(nums) <= 2:
         return False
@@ -12,54 +14,54 @@ def isTripletSequenceExists(nums):
             if nums[j] <= nums[i]:
                 j += 1
                 continue
-        
+
             k = j + 1
             while k < len(nums):
                 if nums[k] > nums[j]:
                     return True
-                
+
                 k += 1
 
-            j += 1        
+            j += 1
         i += 1
-    
+
     return False
 
 
+# Check triplet sequence exits
 def isTripletSequenceExists2(nums):
     if len(nums) < 3:
         return False
-    
+
     numOne, numTwo = math.inf, math.inf
 
     for num in nums:
         if num < numOne:
             numOne = num
-        
+
         if num > numOne:
             numTwo = min(num, numTwo)
-        
+
         if num > numTwo:
             return True
 
     return False
 
 
-
-
 def main():
     # First Approach
-    print(isTripletSequenceExists([1,2,3,4,5]))
-    print(isTripletSequenceExists([5,4,3,2,1]))
-    print(isTripletSequenceExists([2,1,5,0,4,6]))
-    print(isTripletSequenceExists([5,1,5,5,2,5,4]))
-    print(isTripletSequenceExists([1,1,-2,6]))
+    print(isTripletSequenceExists([1, 2, 3, 4, 5]))
+    print(isTripletSequenceExists([5, 4, 3, 2, 1]))
+    print(isTripletSequenceExists([2, 1, 5, 0, 4, 6]))
+    print(isTripletSequenceExists([5, 1, 5, 5, 2, 5, 4]))
+    print(isTripletSequenceExists([1, 1, -2, 6]))
 
     # Second Approach
-    print(isTripletSequenceExists2([1,2,3,4,5]))
-    print(isTripletSequenceExists2([5,4,3,2,1]))
-    print(isTripletSequenceExists2([2,1,5,0,4,6]))
-    print(isTripletSequenceExists2([5,1,5,5,2,5,4]))
-    print(isTripletSequenceExists2([1,1,-2,6]))
+    print(isTripletSequenceExists2([1, 2, 3, 4, 5]))
+    print(isTripletSequenceExists2([5, 4, 3, 2, 1]))
+    print(isTripletSequenceExists2([2, 1, 5, 0, 4, 6]))
+    print(isTripletSequenceExists2([5, 1, 5, 5, 2, 5, 4]))
+    print(isTripletSequenceExists2([1, 1, -2, 6]))
+
 
 main()
---
+++ LeetCode/30-day-challenge/July_2021/July 29th - July 31st/trappingRainWater.py
@@ -0,0 +1,5 @@
+"""
+ Problem Link:- https://leetcode.com/problems/trapping-rain-water/
+ Solution:- SDE_CHEAT_SHEET/Day 7/trappingRainWater.py
+
+"""
