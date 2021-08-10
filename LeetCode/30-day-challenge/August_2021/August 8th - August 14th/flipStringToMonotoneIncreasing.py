47af7bb2b2a9a963a00a5e39c851a4aca2eaacfd
kmameen <ameen.km@hpe.com>
2021-08-10 22:24:48 +0530

    Added one more solution to August 10th 2021 leetcode challenge

--- LeetCode/30-day-challenge/August_2021/August 8th - August 14th/flipStringToMonotoneIncreasing.py
+++ LeetCode/30-day-challenge/August_2021/August 8th - August 14th/flipStringToMonotoneIncreasing.py
@@ -27,3 +27,20 @@ class Solution:
 print(Solution().minFlipsMonoIncr(s="00110"))
 print(Solution().minFlipsMonoIncr(s="010110"))
 print(Solution().minFlipsMonoIncr(s="00011000"))
+
+
+def minFlipsMonoIncrease(s):
+    flipped_zeroes = s.count("0")
+    ones_to_zeroes = 0
+    result = flipped_zeroes
+    for c in s:
+        flipped_zeroes -= c == '0'
+        ones_to_zeroes += c == '1'
+        result = min(result, flipped_zeroes + ones_to_zeroes)
+
+    return result
+
+
+print(minFlipsMonoIncrease(s="00110"))
+print(minFlipsMonoIncrease(s="010110"))
+print(minFlipsMonoIncrease(s="00011000"))
