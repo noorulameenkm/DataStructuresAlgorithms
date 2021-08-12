7e3ef620e3a0bc8b0b22664e7752ba7fddb44c56
kmameen <ameen.km@hpe.com>
2021-08-12 14:46:24 +0530

    Added solution for AUgust 12th 2021 leetcode challenge

--- LeetCode/30-day-challenge/April/April 1st - April 7th/groupAnagrams.py
+++ LeetCode/30-day-challenge/April/April 1st - April 7th/groupAnagrams.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs):
         d = {}
@@ -7,28 +10,29 @@ class Solution:
                 d[sortedstring] = [s]
             else:
                 d[sortedstring].append(s)
-        
+
         results = []
         for value in d.values():
             results.append(value)
-        
+
         return results
-    
+
     def sortedString(self, string):
         return ''.join(sorted(string))
-
 
 
 """
 Time Complexity - O(n * klogk)
 """
+
+
 def anagrams(lst):
     """
     Function to find anagram pairs
     :param lst: A lst of strings
     :return: Group of anagrams
     """
-    
+
     # Write your code here!
     results = []
     dictionary = {}
@@ -40,15 +44,43 @@ def anagrams(lst):
         else:
             dictionary[key] = []
             dictionary[key].append(string)
-    
+
     for _, values in dictionary.items():
         if len(values) >= 2:
             results.append(values)
 
-    results = sorted(results)    
+    results = sorted(results)
     return results
 
 
 arr = ["eat", "tea", "tan", "ate", "nat", "bat"]
 print(Solution().groupAnagrams(arr))
 print(anagrams(arr))
+
+
+def groupAnagrams(strs):
+
+    def construct_char_array(str_):
+        array = [0] * 26
+        for char in str_:
+            index = ord(char) - ord('a')
+            array[index] += 1
+
+        return array
+
+    anagrams = []
+    dict_ = defaultdict(list)
+
+    for s in strs:
+        array = construct_char_array(s)
+        dict_[tuple(array)].append(s)
+
+    for value in dict_.values():
+        anagrams.append(value)
+
+    return anagrams
+
+
+print(groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(groupAnagrams(strs=[""]))
+print(groupAnagrams(strs=["a"]))
---
+++ LeetCode/30-day-challenge/August_2021/August 8th - August 14th/groupAnagrams.py
@@ -0,0 +1,4 @@
+"""
+    Problem Link:- https://leetcode.com/problems/group-anagrams/
+    Solution:- Refer LeetCode/30-day-challenge/April/April 1st - April 7th/groupAnagrams.py
+"""
