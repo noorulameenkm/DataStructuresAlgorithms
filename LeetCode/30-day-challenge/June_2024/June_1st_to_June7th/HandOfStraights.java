0f6032579e8af759a983000eb2cecaf79e65aa88
kmameen <ameen.km@hpe.com>
2024-06-07 11:37:07 +0530

    Added solution for handOfStraights problem

---
+++ LeetCode/30-day-challenge/June_2024/June_1st_to_June7th/HandOfStraights.java
@@ -0,0 +1,53 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.List;
+
+record Input(int[] hand, int groupSize) {}
+
+class HandOfStraights {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> frequency = new HashMap<>();
+        for(int h : hand) {
+            frequency.put(h, frequency.getOrDefault(h, 0) + 1);
+        }
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.addAll(frequency.keySet());
+        int size = pq.size();
+        while(size > 0) {
+            int start = pq.peek();
+            for(int number = start; number < start + groupSize; number++) {
+                if(!frequency.containsKey(number) || frequency.get(number) <= 0) {
+                    return false;
+                }
+                frequency.put(number, frequency.get(number) - 1);
+                if(frequency.get(number) == 0)  {
+                    if(pq.peek() != number) {
+                        return false;
+                    }
+
+                    pq.poll();
+                    size -= 1;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        HandOfStraights handOfStraights = new HandOfStraights();
+        List<Input> iputList = List.of(
+            new Input(new int[]{1,2,3,6,2,3,4,7,8}, 3),
+            new Input(new int[]{1,2,3,4,5}, 4)
+        );
+
+        iputList.forEach((input) -> System.out.println(handOfStraights.isNStraightHand(input.hand(), input.groupSize())));
+    }
+}
