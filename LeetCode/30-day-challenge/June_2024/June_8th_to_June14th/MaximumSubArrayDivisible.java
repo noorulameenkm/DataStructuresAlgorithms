48103a3e88790c571bdc2867c3964bb2e3b494f0
kmameen <ameen.km@hpe.com>
2024-06-08 17:00:11 +0530

    Added solution for maximumsubarrayDivisible

---
+++ LeetCode/30-day-challenge/June_2024/June_8th_to_June14th/MaximumSubArrayDivisible.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+
+record Input(int[] nums, int k) {}
+
+public class MaximumSubArrayDivisible {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> remainders = new HashMap<>();
+        remainders.put(0, -1);
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int remainder = sum % k;
+            if(!remainders.containsKey(remainder)) {
+                remainders.put(remainder, i);
+            } else if(i - remainders.get(remainder) > 1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        MaximumSubArrayDivisible maximumSubArrayDivisible = new MaximumSubArrayDivisible();
+        List<Input> inputs = List.of(
+            new Input(new int[] {23,2,4,6,7}, 6),
+            new Input(new int[] {23,2,6,4,7}, 6),
+            new Input(new int[] {23,2,6,4,7}, 13),
+            new Input(new int[] {23,2,4,6,6}, 7)
+        );
+        
+        inputs.forEach((input) -> System.out.println(maximumSubArrayDivisible.checkSubarraySum(input.nums(), input.k())));
+    }
+
+}
