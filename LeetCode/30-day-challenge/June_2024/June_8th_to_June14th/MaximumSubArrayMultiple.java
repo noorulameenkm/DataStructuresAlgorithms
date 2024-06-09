df7bdf5fc2bc3f89f3410b5c5c7dd9b072292887
kmameen <ameen.km@hpe.com>
2024-06-09 10:45:50 +0530

    Renamed the file name

--- LeetCode/30-day-challenge/June_2024/June_8th_to_June14th/MaximumSubArrayMultiple.java
+++ LeetCode/30-day-challenge/June_2024/June_8th_to_June14th/MaximumSubArrayMultiple.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 record Input(int[] nums, int k) {}
 
-public class MaximumSubArrayDivisible {
+public class MaximumSubArrayMultiple {
     public boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> remainders = new HashMap<>();
         remainders.put(0, -1);
@@ -24,15 +24,15 @@ public class MaximumSubArrayDivisible {
     }
 
     public static void main(String[] args) {
-        MaximumSubArrayDivisible maximumSubArrayDivisible = new MaximumSubArrayDivisible();
+        MaximumSubArrayMultiple maximumSubArrayMultiple = new MaximumSubArrayMultiple();
         List<Input> inputs = List.of(
             new Input(new int[] {23,2,4,6,7}, 6),
             new Input(new int[] {23,2,6,4,7}, 6),
             new Input(new int[] {23,2,6,4,7}, 13),
             new Input(new int[] {23,2,4,6,6}, 7)
         );
-        
-        inputs.forEach((input) -> System.out.println(maximumSubArrayDivisible.checkSubarraySum(input.nums(), input.k())));
+
+        inputs.forEach((input) -> System.out.println(maximumSubArrayMultiple.checkSubarraySum(input.nums(), input.k())));
     }
 
 }
